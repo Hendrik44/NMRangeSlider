bd43eda7448ade823685b94517abf200f19664a3
Hendrik <s42108@beuth-hochschule.de>
2016-05-10 09:47:15 +0200

    add carthage support

--- NMRangeSlider/NMRangeSlider.h
+++ NMRangeSlider/NMRangeSlider.h
@@ -9,6 +9,15 @@
 
 #import <UIKit/UIKit.h>
 
+//! Project version number for NMRangeSlider.
+FOUNDATION_EXPORT double NMRangeSliderVersionNumber;
+
+//! Project version string for NMRangeSlider.
+FOUNDATION_EXPORT const unsigned char NMRangeSliderVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <NMRangeSlider/PublicHeader.h>
+
+
 @interface NMRangeSlider : UIControl
 
 // default 0.0
